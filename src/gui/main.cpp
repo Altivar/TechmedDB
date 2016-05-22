d559feb56a2131977a77a05188ae28b379740a9d
gafillie <goth.fillieres@hotmail.fr>
2016-05-22 11:49:16 +0200

    [ADD] ajout dans les header de l'entête de création fichier/auteur/licence

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,3 +1,28 @@
+/**
+	@File : Main.cpp
+	@Language : c++
+	@Authors :	Fillières Gauthier,
+				Aguilar Axel,
+				Gaillard Christophe
+
+
+	@Licence : GNU General Public License
+
+				This file is part of TechmedDB.
+
+				TechmedDB is free software: you can redistribute it and/or modify
+				it under the terms of the GNU General Public License as published by
+				the Free Software Foundation, either version 3 of the License, or
+				(at your option) any later version.
+
+				TechmedDB is distributed in the hope that it will be useful,
+				but WITHOUT ANY WARRANTY; without even the implied warranty of
+				MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+				See the GNU General Public License for more details.
+
+				You should have received a copy of the GNU General Public License
+				along with TechmedDB ("gpl_3_0.txt"). If not, see <http://www.gnu.org/licenses/>.
+**/
 #include "techmeddb.h"
 #include <QtGui/QApplication>
 
